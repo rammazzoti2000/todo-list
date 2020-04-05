3c3853b58d98d66f01664a56c02f368d574e4992
Alexandru Bangau <rammazzoti2000@gamil.com>
2020-04-05 21:29:28 +0200

    add input  handler to add items to todoList

--- script.js
+++ script.js
@@ -68,5 +68,10 @@ var handlers = {
   },
   toggleAll: function() {
     todoList.toggleAll();
+  },
+  addTodo: function() {
+    var addTodoTextInput = document.getElementById('addTodoTextInput');
+    todoList.addTodo(addTodoTextInput.value);
+    addTodoTextInput.value = '';
   }
 }
