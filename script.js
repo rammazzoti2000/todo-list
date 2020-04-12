589bae4ac7b908a1a7b1b9a1945d10a7d074ef06
Alexandru Bangau <rammazzoti2000@gamil.com>
2020-04-12 22:24:09 +0200

    add completed method for each todo

--- script.js
+++ script.js
@@ -99,7 +99,16 @@ var view = {
     todosUl.innerHTML = '';
     for(let i = 0; i < todoList.todos.length; i++) {
       var todoLi = document.createElement('li');
-      todoLi.textContent = todoList.todos[i].todoText;
+      var todo = todoList.todos[i];
+      var todoTextWithCompletion = '';
+
+      if (todo.completed === true) {
+        todoTextWithCompletion = '(x) ' +  todo.todoText;
+      } else {
+        todoTextWithCompletion = '( ) ' +  todo.todoText;
+      }
+
+      todoLi.textContent = todoTextWithCompletion;
       todosUl.appendChild(todoLi);
     }
   }
